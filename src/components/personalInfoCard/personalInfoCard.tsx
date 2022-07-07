2bf7cfde0b34d0226f9960336e817e2bc763f612
abe <zeamanualfeleke@gmail.com>
2022-07-07 15:41:11 +0300

    personal infor card component completed

--- src/components/personalInfoCard/personalInfoCard.tsx
+++ src/components/personalInfoCard/personalInfoCard.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './personalInfoCard.css'
-
+let userImg = require("../../assets/images/image-jeremy.png")
 function PersonalInfoCard() {
   return (
-    <div>PersonalInfoCard</div>
+    <div className='personal-info-container'>
+      <div className='personal-info'>
+        <div className='img-container'>
+          <img src = {userImg}></img>
+        </div>
+        <div className='name-container'>
+          <p>Report For</p>
+          <p className='name'>Jermy Robson</p>
+        </div>
+
+      </div>
+      <div className='time-container'>
+        <p className='time'>Daily</p>
+        <p className='time'>Weekly</p>
+        <p className='time'>Monthly</p>
+
+      </div>
+    </div>
   )
 }
 
